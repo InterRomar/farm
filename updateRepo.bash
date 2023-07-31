c0992e855b989119febe4c033c9c6c3b5cbfa8b0
InterRomar <kinterov@yandex.ru>
2023-07-31 18:38:02 +0300

    Add additional production instructions to the updateRepo.bash file

--- updateRepo.bash
+++ updateRepo.bash
@@ -17,12 +17,16 @@ if [  -z "$@" ]
 fi
 echo "Working branch " $branch
 
-curl -F chat_id=$TELEGRAM_ADMIN_CHAT -F text="start deploy ${BASE_DOMAIN} ${branch}" \
+curl -F chat_id=$TELEGRAM_ADMIN_CHAT -F text="start deploy ${$SERVICE_DOMAIN}  ${branch}" \
 https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
 
 git pull origin $PROJECTS_BRANCH
 
+rm -rf $PWD/dist
+
+nest build
+
 docker-compose up -d --build
 
-curl -F chat_id=$TELEGRAM_ADMIN_CHAT -F text="finish deploy ${BASE_DOMAIN}" \
+curl -F chat_id=$TELEGRAM_ADMIN_CHAT -F text="finish deploy ${$SERVICE_DOMAIN}" \
 https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
